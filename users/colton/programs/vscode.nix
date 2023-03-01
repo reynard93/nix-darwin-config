ebae01a9a7889c1da10c79759b1f0d35430c3131
Colton Donnelly <colton@donn.io>
2023-03-01 00:38:09 -0800

    [vscode] disable update check

--- users/colton/programs/vscode.nix
+++ users/colton/programs/vscode.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
 	programs.vscode = {
+		# don't auto-enable vscode, as it's a GUI app and
+		# my config should work when headless
 		package = pkgs.vscode;
+
+		enableUpdateCheck = false;
 	};
 }
