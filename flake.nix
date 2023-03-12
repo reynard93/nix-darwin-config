de1dd73c4aa41aaa9382a75fc1e42a8c83947382
Colton Donnelly <colton@donn.io>
2023-03-12 16:02:35 -0700

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -9,10 +9,6 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-		flake-utils = {
-			url = "github:numtide/flake-utils";
-		};
-
 		home-manager = {
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
