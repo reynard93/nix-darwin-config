5fad604761d649355c162a16d325a4f478e6ca8b
Colton Donnelly <colton@donn.io>
2023-03-15 08:24:38 -0700

    [neovim] ok the nvim tree-sitter grammars dont work :(

--- home/colton/programs/neovim.nix
+++ home/colton/programs/neovim.nix
@@ -1,12 +1,13 @@
-{ pkgs, ... }: {
+# { pkgs, ... }:
+{
 	programs.neovim = {
 		enable = true;
 
 		defaultEditor = true;
 
-		plugins = with pkgs.vimPlugins; [
-			nvim-treesitter.withPlugins(_: nvim-treesitter.allGrammars)
-		];
+		# plugins = with pkgs.vimPlugins; [
+		# 	nvim-treesitter.withPlugins(_: nvim-treesitter.allGrammars)
+		# ];
 
 		vimAlias = true;
 		vimdiffAlias = true;
