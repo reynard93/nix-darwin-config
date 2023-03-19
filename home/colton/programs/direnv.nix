ffb5fa025c781ef8d7faae385b71f45d37bfd898
Colton Donnelly <colton@donn.io>
2023-03-19 00:02:39 -0700

    [direnv] auto-enable for my repos, raise warn timeout

--- home/colton/programs/direnv.nix
+++ home/colton/programs/direnv.nix
@@ -5,5 +5,14 @@
 		enableZshIntegration = true;
 
 		nix-direnv.enable = true;
+
+		config.global = {
+			warn_timeout = "1m";
+		};
+
+		config.whitelist.prefix = [
+			"/Users/colton/github.com/cdmistman"
+			"/home/colton/github.com/cdmistman"
+		];
 	};
 }
