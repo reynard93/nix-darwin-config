4907db10fa6c64e76c9730f962138b89d05dab8a
Colton Donnelly <colton@donn.io>
2023-03-19 00:02:52 -0700

    [skim] fix preview option

--- home/colton/programs/skim.nix
+++ home/colton/programs/skim.nix
@@ -6,8 +6,7 @@
 
 		defaultCommand = "${pkgs.fd}/bin/fd --type f || find .";
 		defaultOptions = [
-			"--preview"
-			"${pkgs.bat}/bin/bat --color=always --line-range=:500 {}"
+			"--preview '${pkgs.bat}/bin/bat --color=always --line-range=:500 {}'"
 		];
 	};
 }
