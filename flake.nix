8655824748b5da8eeb310093a9046432c42f390b
Colton Donnelly <colton@donn.io>
2023-03-18 19:42:25 -0700

    [nixpkgs] temporarily set to my branch that updates discord

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "My (cdmistman/colton) Nix config";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+		nixpkgs.url = "github:cdmistman/nixpkgs/discord";
 
 		darwin = {
 			url = "github:LnL7/nix-darwin";
