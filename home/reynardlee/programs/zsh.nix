f2994e07ef8ed4b3e0be6c3ce52cd582ccb208e2
reylee <yylee.rey@gmail.com>
2023-07-20 22:44:38 +0800

    fix zsh.nix

--- home/reynardlee/programs/zsh.nix
+++ home/reynardlee/programs/zsh.nix
@@ -4,12 +4,6 @@
 		enableAutosuggestions = true;
 		enableSyntaxHighlighting = true;
 		autocd = false;
-    loginShellInit = ''
-      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-      fi
-    '';
-
 		defaultKeymap = "viins";
 
 		history = {
